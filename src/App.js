cc49237f39e2d414dfefdbb5a4240a19cc0d6b89
zombico <franz@ombi.co>
2018-09-17 22:49:53 -0400

    refactor for beerlist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './css/App.css';
-
+import seasonalBeers from './data/seasonalBeers';
 
 class App extends Component {
 
@@ -18,29 +18,32 @@ class App extends Component {
     this.showDetails = this.showDetails.bind(this);
     this.getProductDetails = this.getProductDetails.bind(this);
     this.loadMore = this.loadMore.bind(this);
+    this.SetDirectionUrl = this.SetDirectionUrl.bind(this);
   }
   
   componentDidMount() {
-    // get all beer call 
-    fetch("https://lcboapi.com/products?q=beer&per_page=100", {
+    seasonalBeers.map((item) => {
+    fetch(`https://lcboapi.com/products?q=${item.key}`, {
       headers: new Headers({
         'Authorization': 'Token MDphNjBjYWE3Mi1iOTVmLTExZTgtYTI4Ni04YjQzZWRkMTE1NTI6OWhrYUJyQmZhTjZEYmtIN01wR3JUeVNGb1F3V3o2YTJ5UFlT'
       })})
       .then(res => res.json())
       .then(
         (result) => {
+          const beerList = [...this.state.beers] 
+          beerList.push(result.result[0])
           this.setState({
-            isLoaded: true,
-            beers: result.result
-          });
+            beers: beerList
+          })
+          console.log(this.state.beers)
         },
         (error) => {
+          const beerList = [...this.state.beers]
           this.setState({
-            isLoaded: true,
-            error
+            beers: beerList
+          })
           });
-        }
-      )
+    })
   }
 
   showDetails = (beer) => {
@@ -61,7 +64,7 @@ class App extends Component {
       (result) => {
         this.setState({
           availableStores: result.result
-        });
+        })
       },
       (error) => {
         this.setState({
@@ -71,50 +74,64 @@ class App extends Component {
     )
   }
 
+  SetDirectionUrl(address, city) {
+    const mapLoc =  `https://www.google.com/maps/dir//+${address}+${city}`
+    return (
+      mapLoc
+    )
+  }
+
   loadMore() {
     this.setState({
-      beerResultLimit: this.state.beerResultLimit + 12
+      beerResultLimit: this.state.beerResultLimit + 6
     })
   }
 
   render() {
     const { error, isLoaded, beers, cardLoaded, currentCard, availableStores, beerResultLimit } = this.state;
-    console.log(beers)
     return (
       <div className="App">
-        <h1>Beau's Seasonal</h1>
+        <div className="App-header"><div className="App-title">Beau's Seasonal</div></div>
         <div className="main">
-          <div className="left-side">  
+        <div className={currentCard.length === 0 ? "hidden" : "left-side" } >
+              {cardLoaded ? 
+                <div className="preview">
+                  <div className="preview-title">{currentCard.name}</div>
+                  <img className="preview-img" src={currentCard.image_url} />
+                  <div className="preview-entry">{currentCard.style} {currentCard.varietal}</div>
+                  <div className="preview-entry">Alcohol Content: {currentCard.alcohol_content / 100}%</div>
+                  <br/>
+                  <div className="preview-entry">{currentCard.tasting_note}</div>
+                  <br/>
+                  <div className={availableStores.length > 0 ? "hidden" : "beerloader"} onClick={() => this.getProductDetails(currentCard.id)}>Show Available Stores</div>
+                  {
+                    availableStores.map((store) => 
+                    <a target="_blank" className="store-link" href={this.SetDirectionUrl(store.address_line_1, store.city)}>
+                      <div className="store-entry" key={store.id}>
+                        <span>{store.name}</span> <span>{store.telephone}</span>
+                      </div>
+                    </a>
+                    )
+                  }
+                </div>
+                
+                : <div></div>}
+          </div>
+          <div className="right-side">  
               {
-                beers.filter( (beer) => {
-                  if (beer.id === 517797 || beer.id === 169334) {return false} return true
-                })
-                .slice(0, beerResultLimit)
-                .map((beer) => 
-                <div className="beercard" key={beer.id} onClick={() => this.showDetails(beer) }>
-                  <img class="beer-img" src={beer.image_thumb_url}/> 
+                beers.map((beer) => 
+                <div className={currentCard.id === beer.id ? "beercard -selected": "beercard"} key={beer.id} onClick={() => this.showDetails(beer) }>
+                  <img className="beer-img" src={beer.image_thumb_url ? beer.image_thumb_url : "images/error.png"}/> 
                   <div className="beercard-title">{beer.name}</div>
                 </div> 
                 )
               }
               <br/>
-              <div className={beers.length > beerResultLimit ? "beerloader" : "hidden" } onClick={() => this.loadMore()}>Load More</div>
+              <div className={beers.length > beerResultLimit ? "beerloader" : "hidden" } onClick={() => this.loadMore()}>See more beers</div>
           </div>
-          <div className="right-side">
-              {cardLoaded ? 
-                <div>
-                  <div>{currentCard.name}</div>
-                  <div className={availableStores.length > 0 ? "hidden" : ""} onClick={() => this.getProductDetails(currentCard.id)}>Show Available Stores</div>
-                  {
-                    availableStores.map((store) => 
-                      <div key={store.id}>{store.name}</div>
-                    )
-                  }
-                </div>
-                
-                : <div>apple</div>}
-          </div>
+          
         </div>
+        <div className="App-footer"></div>
       </div>
     );
   }
---
+++ src/data/seasonalBeers.js
@@ -0,0 +1,11 @@
+const seasonalBeers =
+[
+    { id:"0", name:"Tom Green Summer Stout", key:"Tom+Green+Summer+Stout" },
+    { id:"1", name:"The Tom Green Beer", key:"The+Tom+Green+Beer"},
+    { id:"2", name:"Full Time IPA", key:"full+time+ipa"},
+    { id:"3", name:"Hopfenlager", key:"Hopfenlager" },
+    { id:"4", name:"Halcyon Barrel House - Gravity Well", key:"Halcyon+Barrel+House+Gravity+Well"},
+    { id:"5", name:"Halcyon Barrel House - Infinity Mirror", key:"Halcyon+Barrel+House+Infinity+Mirror"}    
+]
+
+export default seasonalBeers;
